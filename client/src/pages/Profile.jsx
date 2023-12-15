4ddd6a44cffb4b0b2d4fbe999cc8150f08d1d318
Nihad Adnan <nihad08adnan@gmail.com>
2023-12-15 10:59:08 +0600

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux"
 import { useRef, useState , useEffect} from "react"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart,updateUserSuccess,updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart,updateUserSuccess,updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -73,6 +73,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message))
     }
   }
+
+  const handleDelete=async()=>{
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure(data.message))
+        return
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl text-center font-bold my-9'>Profile</h1>
@@ -117,7 +135,7 @@ export default function Profile() {
         <button disabled={loading} className="bg-gray-800 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-70">{loading ? 'Loading.....' : 'Update'}</button>
         </form>
         <div className="flex justify-between mt-5">
-          <span className="text-red-700 font-semibold cursor-pointer">Delete Account</span>
+          <span onClick={handleDelete} className="text-red-700 font-semibold cursor-pointer">Delete Account</span>
           <span className="text-red-700 font-semibold cursor-pointer">Sign Out</span>
         </div>
 
